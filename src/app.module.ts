54164af8eaaac0f54194fa55b100d277c8712dcf
Deki <dekiakbar@icloud.com>
2023-03-05 15:31:32 +0700

    postgress docker, config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.example'],
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ]
 })
 export class AppModule {}
